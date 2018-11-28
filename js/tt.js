d149967d0e1c0485f9609c61d620dea192ecd9d7
Lily Wu <sunturnip@lilys-mbp.dhcp.wustl.edu>
2018-11-28 17:13:14 -0600

    labels

--- js/tt.js
+++ js/tt.js
@@ -3,40 +3,29 @@ function tt() {
     var scatterChart = new Chart(ctx, {
         type: 'scatter',
        data: {
-          labels: ['"Bbibbi" by IU', '"Sorry" by Young Da Il', '"Hello Tutorial" by Zion. T', '"Fall in Fall" by VIBE', '"There Has Never Been A Day I Haven\'t Loved You" by Im Chang Jung', '"Good Day" by Loopy x Kid Milli x pH - 1', '"It Takes Time" by LOCO', '"Save" by Loopy', '"Every Day, Every Moment" by Paul Kim', '"Way Back Home" by SHAUN'],
+          labels: ['"Bbibbi" by IU', 
+                   '"Sorry" by Young Da Il', 
+                   '"Hello Tutorial" by Zion. T', 
+                   '"Fall in Fall" by VIBE', 
+                   '"There Has Never Been A Day I Haven\'t Loved You" by Im Chang Jung', 
+                   '"Good Day" by Loopy x Kid Milli x pH - 1', 
+                   '"It Takes Time" by LOCO', '"Save" by Loopy', 
+                   '"Save" by Loopy',
+                   '"Every Day, Every Moment" by Paul Kim', 
+                   '"Way Back Home" by SHAUN'],
           datasets: [{
              label: 'Major K-Pop Chords',
              backgroundColor: '#4688f1',
-             data: [{
-                    x: 0.15,
-                    y: 1
-                }, {
-                    x: 0.19791666666666666,
-                    y: 2
-                }, {
-                    x: 0.3333333333333333,
-                    y: 3
-                }, {
-                    x: 0.16551724137931034,
-                    y: 4
-                }, {
-                    x: 0.21153846153846154,
-                    y: 5
-                }, {
-                    x: 0.08176100628930817,
-                    y: 6
-                }, {
-                    x: 0.15079365079365079,
-                    y: 7
-                }, {
-                    x: 0.06565656565656566,
-                    y: 8
-                }, {
-                    x: 0.08247422680412371,
-                    y: 9
-                }, {
-                    x: 0.030864197530864196,
-                    y: 10
+             data: [{x: 1, y: 0.15}, 
+                    {x: 2, y: 0.19791666666666666}, 
+                    {x: 3, y: 0.3333333333333333}, 
+                    {x: 4, y: 0.16551724137931034},
+                    {x: 5, y: 0.21153846153846154}, 
+                    {x: 6, y: 0.08176100628930817}, 
+                    {x: 7, y: 0.15079365079365079}, 
+                    {x: 8, y: 0.06565656565656566}, 
+                    {x: 9, y: 0.08247422680412371}, 
+                    {x: 10, y: 0.030864197530864196
              }]
           }]
        },
@@ -48,6 +37,20 @@ function tt() {
                    return label + '\n(' + tooltipItem.xLabel + ', ' + tooltipItem.yLabel + ')';
                 }
              }
+          },
+          scales: {
+            xAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: 'Chart Ranking'
+              }
+            }],
+            yAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: 'Type Token'
+              }
+            }]
           }
        }
     });
